1e9bb48a406cb4b36b7fcf2a65e03ed6af419c10
Sander Stad <sander@sqlstad.nl>
2024-02-28 14:32:29 +0100

    Added tests

---
+++ tests/functions/Get-PSBIActivityEvent.Tests.ps1
@@ -0,0 +1,42 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    BeforeAll {
+        $CommandName = 'Get-PSBIActivityEvent'
+    }
+
+    Context "Validate known parameters" {
+        It "Should only contain our specific parameters" {
+            [string[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+
+            [string[]]$knownParameters = 'StartDate', 'EndDate', 'Operation', 'UserId', 'ClientIP', 'EnableException'
+            $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should -Be 0
+        }
+
+        It "Should have the correct parameters" {
+            Get-Command $CommandName | Should -HaveParameter StartDate -Type datetime
+            Get-Command $CommandName | Should -HaveParameter EndDate -Type datetime
+            Get-Command $CommandName | Should -HaveParameter Operation -Type string
+            Get-Command $CommandName | Should -HaveParameter UserId -Type string
+            Get-Command $CommandName | Should -HaveParameter ClientIP -Type string
+            Get-Command $CommandName | Should -HaveParameter EnableException -Type switch
+        }
+    }
+}
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+
+    BeforeAll {
+
+    }
+
+    Context "Tests" {
+
+    }
+
+    AfterAll {
+
+    }
+}
